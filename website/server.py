2aed8cf27c0d1afcd7e4428b5629566b48166d29
Larry Gates <gateslm@rose-hulman.edu>
2017-04-24 20:34:07 -0400

    Added more to server
    Might generate the page I want

--- website/server.py
+++ website/server.py
@@ -9,11 +9,12 @@ import redis
 import json
 from bson.objectid import ObjectId
 import ast
+import generate_schedule_html_table.py
 
 
 mongoClient = MongoClient() # TODO: Add the connection info
 monetClient = connections.monetConn() # TODO: Add the connection info
-redisClient = redis.Redis() # TODO: Add the connection info
+redisClient = redis.Redis(host='moneteam-1.csse.rose-hulman.edu', port=6379) # TODO: Add the connection info
 app = Flask(__name__)
 
 
@@ -23,7 +24,24 @@ def index():
 
 @app.route('/schedule_generator')
 def schedule_generator():
-     return '<html><head></head><body>Schedule_generator</body></html>'
+     week_id = 1
+     employee_name = "james"
+
+    # opening_time = auto_scheduler.openning_time
+    # closing_time = auto_scheduler.closing_time
+
+     opening_time =8
+     closing_time = 22
+
+     schedule = import_schedule(employee_name,week_id)
+     schedule = np.transpose(schedule)
+     df = pd.DataFrame(schedule,columns = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"])
+     df.index = range(opening_time,closing_time)
+
+     html_table = df.to_html()
+     html_table = re.sub("False","",html_table)
+     html_table = re.sub("True","&#10004",html_table)
+     return html_table
 
 @app.route('/employee_settings')
 def employee_settings():
