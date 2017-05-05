4fcc60f54202e618817897d50b07c32da44d0ec7
edwardjh <edwardjh@rose-hulman.edu>
2017-05-04 20:02:30 -0500

    start function to send back new prefs

--- website/static/change_preferences.js
+++ website/static/change_preferences.js
@@ -17,7 +17,6 @@ function checkPreferenceBoxes() {
                 var x = preference['day'] + "." + preference[
                     'hour'];
                 var element = document.getElementById(x.toString());
-                //element.innerHTML = '<input type="checkbox">';
                 element.checked = true;
                 element.onchange = function() {
                     console.log("On Change" + x);
@@ -28,4 +27,17 @@ function checkPreferenceBoxes() {
 
 function submitScheduleRequest() {
      // TODO: Do me James!!!
+        var days = [1,2,3,4,5,6,7];
+	var hours = [8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+	var checkedArray = [];
+	for (d = 1; d <= 7; d++) {
+		for (h = 8; h <= 21; h++) {
+			var x = d + "." + h;
+			var elem = document.getElementById(x);
+			console.log(elem.checked);
+			checkedArray.push(elem);
+		}
+	}
+
+
 }
