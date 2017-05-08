68d3a5d26f2b8d1eeef8270cad46afb7812913bd
Ruinan Zhang <zhangr4@rose-hulman.edu>
2017-05-08 19:43:33 -0400

    update push history

--- website/push_history.py
+++ website/push_history.py
@@ -1,6 +1,44 @@
-
-
 import employeefunctions
+
+from scheduler import mongo_connect
+from scheduler import generate_schedule_html_table
+from scheduler import redis_conn
+from scheduler import parameters
+from scheduler import redis_connect
+import numpy as np
+import pandas as pd
+
+def get_redis_history(week_id,employee):
+    conn = redis_conn.redisConn()
+    schedule_raw = generate_schedule_html_table.import_schedule(employee,week_id,conn)
+    schedule = np.transpose(schedule_raw)
+    df = pd.DataFrame(schedule,columns = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"])
+    opening_time = parameters.opening_time
+    closing_time = parameters.closing_time
+    df.index = range(opening_time,closing_time+1)
+    return df
+
+
+def import_history(week_id):
+    employees = parameters.get_current_employees(week_id)
+    mongoClient = mongo_connect.mongoConn()
+
+    db = mongoClient['employee_history']
+
+
+    e = employees[0]
+    df= get_redis_history(week_id,e,)
+
+    key = str(e)+"_"+str(week_id)
+    doc = {"key":key,"schedule":df.to_json()}
+
+    db.insert_one(doc)
+    # print doc
+
+import_history(1)
+
+
+
 
 
 def put_weeknum_into_monet(weeknum):
