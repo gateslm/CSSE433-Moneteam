80de28ec06e47334a4b17642870bd4a4a5ba53fc
gateslm <gateslm@rose-hulman.edu>
2017-05-11 15:09:22 -0500

    je fix put_weeknums_into_monet

--- website/push_history.py
+++ website/push_history.py
@@ -9,6 +9,7 @@ from scheduler import redis_connect
 import numpy as np
 import pandas as pd
 import json
+import ast
 from pymongo import errors
 
 def get_redis_history(week_id,employee):
@@ -57,17 +58,21 @@ def import_history(week_id):
 def put_weeknum_into_monet(weeknum):
     q1  = "select json.filter(weeknums, \'vals\') from weeknumtable; "
     count, res = employeefunctions.executeEmpQueryCursorAll(q1)
-    # print("select filter weeknums count", count)
-    # print("res[0]", res[0])
-    x = res[0]
-    x.append(weeknum)
-    y = {'vals': x}
-    # print(y)
+    #print(res)
+    #print(res[0])
+    #print(res[0][0])
+    aa = ast.literal_eval(res[0][0])[0]
+    #print(aa)
+    #print(type(aa))
+    aa.append(weeknum)
+    y = {'vals': aa}
     z = employeefunctions.getMonetConvertedVal(json.dumps(y))
-    q2 = "update weeknumtable set weeknums = z;"
+    q2 = "update weeknumtable set weeknums = %s;" % z
+    print("q2 = ", q2)
     conn3 = employeefunctions.getMC3()
     c2 = conn3.execute(q2)
-    # print("count of execute update", c2)
+    conn3.commit()
+    print("count of execute update", c2)
     return True
 
 
@@ -93,3 +98,9 @@ def delete_weeknums_in_monet():
 
 # print(employeefunctions.getMonetConvertedVal({'vals':[1,2,3]}))
 # delete_weeknums_in_monet()
+# put_weeknum_into_monet(99)
+# put_weeknum_into_monet(98)
+# put_weeknum_into_monet(97)
+# put_weeknum_into_monet(1)
+# put_weeknum_into_monet(2)
+# put_weeknum_into_monet(3)
